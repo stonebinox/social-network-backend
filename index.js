b0c310970ce6593d226a03904cf59ea6af759492
Anoop Santhanam <anoop.santhanam@gmail.com>
2024-04-14 17:45:37 +0530

    Added route to get user info

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const userController = new UserController();
 app.use(cors());
 app.post("/user", userController.createOrlogin.bind(userController));
 app.get("/users", userController.getUsers.bind(userController));
+app.get("/user", userController.getUser.bind(userController));
 
 app.listen(port, () => {
   console.log(`server running at http://localhost:${port}`);
--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,6 +1,13 @@
 import User from "../models/user";
 
 export default class UserController {
+  /**
+   * Creates or returns user info
+   *
+   * @param {Request} req
+   * @param {Response} res
+   * @returns {Response}
+   */
   async createOrlogin(req, res) {
     const {
       query: { username = null },
@@ -33,6 +40,13 @@ export default class UserController {
     });
   }
 
+  /**
+   * Gets all users
+   *
+   * @param {Request} req
+   * @param {Response} res
+   * @returns {Response}
+   */
   async getUsers(req, res) {
     const user = new User();
     const users = await user.getUsers();
@@ -40,6 +54,42 @@ export default class UserController {
     return res.status(200).send({
       success: 200,
       data: users,
+    });
+  }
+
+  /**
+   * Gets a user
+   *
+   * @param {Request} req
+   * @param {Response} res
+   * @returns {Response}
+   */
+  async getUser(req, res) {
+    const {
+      query: { username = null },
+    } = req;
+
+    if (!username)
+      return res.status(400).send({
+        success: false,
+        error: "Invalid username",
+      });
+
+    const parsedUsername = encodeURI(username).toLowerCase(); // basic sanotisation
+
+    const user = new User();
+    let userData = await user.getUser(parsedUsername);
+
+    if (!userData) {
+      return res.status(400).send({
+        success: false,
+        error: "No user found",
+      });
+    }
+
+    return res.status(200).send({
+      success: true,
+      data: userData,
     });
   }
 }
